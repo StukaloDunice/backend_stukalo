b4fee89fd609c64906f0a747858e160ecf000c40
Stanislav Stukalo <s.stukalo@dunice.net>
2022-03-18 09:50:51 +0300

    append header for response, and append in model as:user

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -5,11 +5,13 @@ const BAD_REQUEST = 400;
 
 module.exports = {
   getNews(req, res) {
-    News.findAll({ include: Users })
+    News.findAll({ include: { model: Users, as: 'user' } })
       .then((news) => {
+        res.header('Access-Control-Allow-Origin', '*');
         res.status(OK).send(news);
       })
       .catch((error) => {
+        res.header('Access-Control-Allow-Origin', '*');
         res.status(BAD_REQUEST).send(error);
       });
   },
