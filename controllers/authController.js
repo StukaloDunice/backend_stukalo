bdb0cac3f5572c22c6b55b61c79e59f782af57ea
Stanislav Stukalo <s.stukalo@dunice.net>
2022-03-21 23:15:10 +0300

    append jsonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const { Users } = require('../models');
+const token = require("jsonwebtoken");
+const { Users } = require("../models");
 
 const OK = 200;
 const BAD_REQUEST = 400;
@@ -6,8 +7,8 @@ const BAD_REQUEST = 400;
 module.exports = {
   registrationUser(req, res) {
     Users.create(req.body)
-      .then((news) => {
-        res.status(OK).send(news);
+      .then((user) => {
+        res.status(OK).send(user);
       })
       .catch((error) => {
         res.status(BAD_REQUEST).send(error);
